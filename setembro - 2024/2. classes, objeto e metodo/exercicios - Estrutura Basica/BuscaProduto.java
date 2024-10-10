3f2fc87fd3b983ccd2dd37ef248b0977f345d6ad
ssnaligia <ana.ligia@aluno.ifsp.edu.br>
2024-10-09 21:20:07 -0300

    exerciicioss

--- setembro - 2024/2. classes, objeto e metodo/exercicios - Estrutura Basica/BuscaProduto.java
+++ setembro - 2024/2. classes, objeto e metodo/exercicios - Estrutura Basica/BuscaProduto.java
@@ -7,6 +7,30 @@ Código Produto Valor
 2 cappucino R$ 9,50
 3 chocolate R$ 10,20 */
 
+import java.util.Scanner;
+
 class BuscaProduto {
-    
+    public static void main(String[] args) {
+        int codigo;
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite o código do produto: ");
+        codigo = entrada.nextInt();
+        
+        switch (codigo) {
+            case 1: 
+                System.out.println("Produto: Café | Valor: R$ 5,00");
+                break;
+            case 2: 
+                System.out.println("Produto: Cappuccino | Valor: R$ 9,50");
+                break;
+            case 3: 
+                System.out.println("Produto: Chocolate | Valor: R$ 10,20");
+                break;
+            default: 
+                System.out.println("Produto não cadastrado.");
+        }
+        
+        entrada.close();
+    }
 }
