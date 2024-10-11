2ba8162fda0a586ce1f693b7df2dc73db2def6ec
ssnaligia <ana.ligia@aluno.ifsp.edu.br>
2024-10-11 13:45:35 -0300

    exercicioss

---
+++ 2. outubro - 2024/1. classes, objeto e metodo/exercicios - Estrutura Basica/Eleicao.java
@@ -0,0 +1,25 @@
+/*Em uma eleição municipal existem três candidatos. Os votos são informados através de códigos. 
+Os dados utilizados para a contagem dos votos obedecem às seguintes regras:    
+- 1,2,3: voto para os respectivos candidatos;  
+- 4: voto nulo;  
+- 5: voto em branco; 
+Elabore um programa que leia o código do candidato, calcule e mostre: 
+ - total de votos para cada candidato; 
+- total de votos nulos;  
+- total de votos em branco; 
+Para encerrar o programa, a opção “0” deve ser informada. */
+
+import java.util.Scanner;
+
+class Eleicao {
+    public static void main(String[] args) {
+        int voto;
+
+        Scanner entrada = new Scanner(System.in);
+        do {
+            System.out.print("\n\n--- URNA ELETRÔNICA ---");
+            System
+        }
+
+    }
+}
